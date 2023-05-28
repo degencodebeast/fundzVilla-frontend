d759db94fc8eb991e107bcf4f9775b40c7600333
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-05-28 04:51:30 +0100

    fix

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -4,7 +4,7 @@ import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
-  useSystemColorMode: false,
+  useSystemColorMode: true,
 }
 
 
