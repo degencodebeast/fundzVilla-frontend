d1dd7d4245e80845a15d503220b89837b7e43ec7
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-05-28 14:26:33 +0100

    updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,22 +2,22 @@ import { Stack, HStack, Link, Divider, Image, IconButton, LinkProps } from '@cha
 // Here we have used react-icons package for the icons
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
-const links = [ 'White paper'];
+const links = [ 'White papers'];
 const accounts = [
   {
-    url: 'https://github.com/MA-Ahmad/templateskart',
+    url: '#',
     label: 'Github Account',
     type: 'gray',
     icon: <FaGithub />
   },
   {
-    url: 'https://twitter.com/muhammad_ahmaad',
+    url: '#',
     label: 'Twitter Account',
     type: 'twitter',
     icon: <FaTwitter />
   },
   {
-    url: 'https://linkedin.com/in/muhammad-ahmad20',
+    url: '#',
     label: 'LinkedIn Account',
     type: 'linkedin',
     icon: <FaLinkedin />
@@ -49,19 +49,7 @@ const Footer = () => {
 
       {/* Mobile and Tablet Screens */}
       <Stack display={{ base: 'flex', md: 'none' }} alignItems="center">
-        <HStack alignItems="center">
-          <CustomLink>Sign up</CustomLink>
-          <Divider h="1rem" orientation="vertical" />
-          <CustomLink>Blog</CustomLink>
-          <Divider h="1rem" orientation="vertical" />
-          <CustomLink>Career</CustomLink>
-        </HStack>
-        <HStack alignItems="center">
-          <CustomLink>Documentation</CustomLink>
-          <Divider h="1rem" orientation="vertical" />
-          <CustomLink>Terms of use</CustomLink>
-        </HStack>
-        <CustomLink>Privacy policy</CustomLink>
+       
       </Stack>
 
       <Stack direction="row" spacing={5} pt={{ base: 4, md: 0 }} alignItems="center">
